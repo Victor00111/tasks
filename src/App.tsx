8198b77bc84f8351d67abdf48123efcddd1063c3
vtung@udel.edu <vtung@udel.edu>
2023-02-17 23:10:03 -0500

    Added button and red columns

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
 
 function App(): JSX.Element {
@@ -8,19 +9,29 @@ function App(): JSX.Element {
                 UD CISC275 with React Hooks and TypeScript
             </header>
             <h1>This is the header.</h1>
-            <img src="C:\Users\Victo\tasks\src\images\TurtlePic.jfif" alt="A picture of a turtle"/>
+            <img
+                src="C:\Users\Victo\tasks\src\images\TurtlePic.jfif"
+                alt="A picture of a turtle"
+            />
             <ol>
                 <li>First</li>
                 <li>Second</li>
                 <li>Third</li>
             </ol>
-            
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
-            <p> Victor Tung </p>
-            <p> Hello World </p>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
+            <div className="2-columns">
+                <Container>
+                    <Row>
+                        <div className="red-columns">
+                            <Col>First column.</Col>
+                            <Col>Second column.</Col>
+                        </div>
+                    </Row>
+                </Container>
+            </div>
+            <p>Victor Tung</p>
         </div>
     );
 }
